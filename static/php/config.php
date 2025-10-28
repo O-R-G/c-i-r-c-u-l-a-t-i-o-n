a4bc49a74c64c7411864c6babdc8495aea6c5bfc
wei <wei@o-r-g.com>
2025-10-28 14:45:01 +0800

    update screenfull and config.php

--- static/php/config.php
+++ static/php/config.php
@@ -2,8 +2,14 @@
 /*
     site-specific config
 */
-
+$site_title = 'C-i-r-c-u-l-a-t-i-o-n';
 $head = 'VIS 217';
 $site = 'C-i-r-c-u-l-a-t-i-o-n';
 $home = $head . ", " . $site;
+$card_default = '/media/jpg/card-default.jpg';
+$logo_src = '/media/jpg/logo.jpg';
+$description = 'Hello, w-w-w-orld.';
+$site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+$site_url .= '://' . $_SERVER['SERVER_NAME'];
+$og_locale = 'en_US';
 ?>
